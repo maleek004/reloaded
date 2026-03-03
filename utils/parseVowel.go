9f6a1df4dfd59998f4b2005272e72a659a8bac6e
mabdulmal <mubaraqabdulmaleek@gmail.com>
2026-03-03 14:14:04 +0100

    added parsevowel

--- utils/parseVowel.go
+++ utils/parseVowel.go
@@ -1,13 +1,24 @@
 package utils
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+)
 
 func ParseVowel(index int, text []string) {
 	if index == len(text)-1 {
 		return
 	}
+	var nextWord []rune
+	for i := index + 1; i < len(text); i++ {
+		if !strings.HasPrefix(text[i], "(") && !strings.HasSuffix(text[i], ")") {
+			nextWord = []rune(text[i])
+			break
 
-	nextWord := []rune(text[index+1])
+		}
+
+	}
+
 	char := unicode.ToLower(nextWord[0])
 
 	switch char {
